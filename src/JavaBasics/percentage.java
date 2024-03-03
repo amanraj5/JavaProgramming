6a572989d352a4940f16115b345d89c983cd4d62
Aman Raj <AMANRAJSINGH806@GMAIL.COM>
2024-03-03 21:24:46 +0530

    percentage and basics

---
+++ src/JavaBasics/percentage.java
@@ -0,0 +1,14 @@
+package JavaBasics;
+
+public class percentage {
+    public static void main(String[] args) {
+        double math=92;
+        double science=96;
+        double sst=78;
+        double english=89;
+        double sanskrit=88;
+
+        double percentage=(math+sanskrit+science+sst+english)/5;
+        System.out.println("The percentage is "+percentage);
+    }
+}
---
+++ src/JavaBasics/sumof2.java
@@ -0,0 +1,8 @@
+package JavaBasics;
+
+public class sumof2 {
+    public static void main(String[] args) {
+        int x=23,y=45;
+        System.out.println(x+y);
+    }
+}
